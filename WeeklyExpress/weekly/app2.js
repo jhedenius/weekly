1e000c516b72f3264eb6e4e80ac890c965027577
Joachim Hedenius <joachim@hedenius.se>
2012-05-26 14:34:11 +0200

    facebook login wip

--- WeeklyExpress/weekly/app2.js
+++ WeeklyExpress/weekly/app2.js
@@ -94,5 +94,5 @@ app.get('/logout', function( request, response, params ) {
 
 });
 
-app.listen(2000);
+app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
